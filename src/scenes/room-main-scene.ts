e7354212fa2921151c34a95cb5dc3577001d6fe4
Daniel Riissanen <daniel.riissanen@gmail.com>
2021-04-10 15:15:20 +0300

    feat: Add resource labels

--- src/scenes/room-main-scene.ts
+++ src/scenes/room-main-scene.ts
@@ -1,4 +1,4 @@
-import { Engine, Scene, vec } from 'excalibur';
+import { Color, Engine, FontStyle, Label, Scene, vec } from 'excalibur';
 import { GameState } from '../game-state';
 import {
   Background,
@@ -7,6 +7,7 @@ import {
   RoomGreenIcon,
   RoomRedIcon
 } from '../actors';
+import { LabelArgs } from 'excalibur/dist/Label';
 
 /**
  * Managed scene
@@ -20,6 +21,9 @@ export class RoomMainScene extends Scene {
   private roomGreen: RoomGreenIcon;
   private roomBlue: RoomBlueIcon;
   private button: AssignButton;
+  private redResourceLabel: Label;
+  private greenResourceLabel: Label;
+  private blueResourceLabel: Label;
 
   public constructor(engine: Engine, state: GameState) {
     super(engine);
@@ -28,25 +32,11 @@ export class RoomMainScene extends Scene {
   }
 
   public onActivate() {
-    this.background = new Background(this.engine, this.state);
-    this.background.pos = vec(this.engine.drawWidth / 2, this.engine.drawHeight / 2);
-    this.add(this.background);
+    this.createBackground();
+    this.createRoomIcons();
+    this.createUIButton();
+    this.createUITexts();
 
-    this.roomRed = new RoomRedIcon(this.engine, this.state);
-    this.roomRed.pos = vec(200, 200);
-    this.add(this.roomRed);
-
-    this.roomGreen = new RoomGreenIcon(this.engine, this.state);
-    this.roomGreen.pos = vec(600, 200);
-    this.add(this.roomGreen);
-
-    this.roomBlue = new RoomBlueIcon(this.engine, this.state);
-    this.roomBlue.pos = vec(1000, 200);
-    this.add(this.roomBlue);
-
-    this.button = new AssignButton(this.engine, this.state);
-    this.button.pos = vec(this.engine.drawWidth / 2, this.engine.drawHeight - 100);
-    this.add(this.button);
   }
 
   public onDeactivate() {
@@ -54,5 +44,65 @@ export class RoomMainScene extends Scene {
     this.remove(this.roomGreen);
     this.remove(this.roomBlue);
     this.remove(this.button);
+    this.remove(this.redResourceLabel);
+    this.remove(this.greenResourceLabel);
+    this.remove(this.blueResourceLabel);
+  }
+
+  private createBackground(): void {
+    this.background = new Background(this.engine, this.state);
+    this.background.pos = vec(this.engine.drawWidth / 2, this.engine.drawHeight / 2);
+    this.add(this.background);
+  }
+
+  private createRoomIcons(): void {
+    this.roomRed = new RoomRedIcon(this.engine, this.state);
+    this.roomRed.pos = vec(200, 300);
+    this.add(this.roomRed);
+
+    this.roomGreen = new RoomGreenIcon(this.engine, this.state);
+    this.roomGreen.pos = vec(600, 300);
+    this.add(this.roomGreen);
+
+    this.roomBlue = new RoomBlueIcon(this.engine, this.state);
+    this.roomBlue.pos = vec(1000, 300);
+    this.add(this.roomBlue);
+
+  }
+
+  private createUIButton(): void {
+    this.button = new AssignButton(this.engine, this.state);
+    this.button.pos = vec(this.engine.drawWidth / 2, this.engine.drawHeight - 100);
+    this.add(this.button);
+  }
+
+  private createUITexts(): void {
+    const commonFontConfig: LabelArgs = {
+      fontFamily: 'Arial',
+      fontSize: 30,
+      bold: true,
+      color: Color.White
+    };
+
+    this.redResourceLabel = new Label({
+      ...commonFontConfig,
+      text: 'Red: 0',
+      pos: vec(50, 60)
+    });
+    this.add(this.redResourceLabel);
+
+    this.greenResourceLabel = new Label({
+      ...commonFontConfig,
+      text: 'Green: 0',
+      pos: vec(50, 95)
+    });
+    this.add(this.greenResourceLabel);
+
+    this.blueResourceLabel = new Label({
+      ...commonFontConfig,
+      text: 'Blue: 0',
+      pos: vec(50, 130)
+    });
+    this.add(this.blueResourceLabel);
   }
 }
